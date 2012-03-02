0130a6e7dbec115d2e2836f99e9db2d15a4052b3
cingusoft <cingusoft@gmail.com>
2012-03-02 03:03:01 +0100

    little change

---
+++ django_phonehub/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'Andrea Mucci'
+__copyright__ = 'Copyright 2012, Andrea Mucci'
+__credits__ = ['Andrea Mucci, cingusoft.posterous.com, @cingusoft']
+
+
+__license__ = 'BSD'
+__version__ = '0.0.1'
+__maintainer__ = 'Andrea Mucci'
+__email__ = 'cingusoft@gmail.com'
+__status__ = 'Production'
+
